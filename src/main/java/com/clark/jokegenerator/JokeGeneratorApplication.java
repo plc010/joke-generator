baf78e63c1c2489349de0f2ff07a706f9bc65955
plc010 <shaker72@icloud.com>
2018-07-05 10:19:41 -0400

    XML config

--- src/main/java/com/clark/jokegenerator/JokeGeneratorApplication.java
+++ src/main/java/com/clark/jokegenerator/JokeGeneratorApplication.java
@@ -2,8 +2,10 @@ package com.clark.jokegenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class JokeGeneratorApplication {
 
     public static void main(String[] args) {
